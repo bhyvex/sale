c733689fcd4d87d8d5e2c5dfa18f59bd750ce305
Donovan Dikaio <i@dikaio.com>
2017-10-16 22:43:21 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple landing page for domains listed for sale on [Sedo](https://www.sedo.com/).
+
+`hub clone dikaio/sale`
+
+Update domain.tld to your domain.
---
+++ source/assets/javascripts/app.js
@@ -0,0 +1,8 @@
+//=require 'plugins/jquery1.11.2.min'
+//=require 'plugins/bootstrap.min'
+//=require 'plugins/scrollreveal.min'
+//=require 'plugins/waterpipe'
+
+//=require 'plugins/prefixfree.min'
+
+//=require 'custom'
